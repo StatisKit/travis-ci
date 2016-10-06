a2c58ee2344ab3eeb7e9bc366a1f83985e994632
Pierre Fernique <pfernique@gmail.com>
2016-10-06 17:02:47 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+set -xe
+
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
  elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
