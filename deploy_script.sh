3b8bcbab6aa067f9b59bdf668d204d5ac9be3a44
Pierre Fernique <pfernique@gmail.com>
2017-01-22 11:31:23 +0100

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -6,9 +6,14 @@ fi
 
 if [[ "$ANACONDA_DEPLOY" = "true" ]]; then
   if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+    if [[ ! "$ENVIRONMENT" = "" ]]; then
+      anaconda upload $ENVIRONMENT -u statiskit --force
+    fi
     md5sum `conda build ../conda/$RECIPE -c conda-forge -c statiskit --output`
   fi
-  anaconda upload `conda build ../conda/$RECIPE -c conda-forge -c statiskit --output` -u statiskit --force
+  if [[ ! "$RECIPE" = "" ]]; then
+    anaconda upload `conda build ../conda/$RECIPE -c conda-forge -c statiskit --output` -u statiskit --force
+  fi
 fi
 
 set +ev
