04cbd3b53cd7942530c06362d54f66e489099eba
Pierre Fernique <pfernique@gmail.com>
2017-01-22 11:29:09 +0100

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,7 +2,7 @@ set -e
 set +v
 
 DOCKER_DEPLOY="false"
-if [[ "$TRAVIS_OS_NAME" = "linux" &&  ! "$DOCKER_USERNAME" = "" && ! "$DOCKER_PASSWORD" = "" && "$RECIPE" = "" ]]; then
+if [[ "$TRAVIS_OS_NAME" = "linux" &&  ! "$DOCKER_USERNAME" = "" && ! "$DOCKER_PASSWORD" = "" && "$RECIPE" = "" && "$ENVIRONMENT" = "" ]]; then
     DOCKER_DEPLOY="true"
 fi
 if [[ "$DOCKER_DEPLOY" = "true" ]]; then
@@ -11,8 +11,10 @@ fi
 export DOCKER_DEPLOY=$DOCKER_DEPLOY
 
 ANACONDA_DEPLOY="false"
-if [[ ! "$ANACONDA_USERNAME" = "" && ! "$ANACONDA_PASSWORD" = "" && ! "$RECIPE" = "" ]]; then
-    ANACONDA_DEPLOY="true"
+if [[ ! "$ANACONDA_USERNAME" = "" && ! "$ANACONDA_PASSWORD" = "" ]]; then
+    if [[ ! "$RECIPE" = "" || ! "$ENVIRONMENT" = "" ]]; then
+        ANACONDA_DEPLOY="true"
+    fi
 fi
 if [[ "$ANACONDA_DEPLOY" = "true" ]]; then
   yes | anaconda login --password $ANACONDA_PASSWORD --username $ANACONDA_USERNAME
