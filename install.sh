2183bb5e3384561a1b4a8fec8580557249727b17
Pierre Fernique <pfernique@gmail.com>
2017-02-01 09:35:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 set -ev
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+  if [[ "$TRAVIS_BRANCH" = "linux32" ]]; then
+    export PLATFORM="x86"
+    sudo dpkg --add-architecture i386
+    set +e
+    sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+    sudo apt-get install multiarch-support
+    set -e
+  else
+    export PLATFORM="x86_64"
+  fi
   sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   sudo apt-get update
   sudo apt-get install -qq gcc-5 g++-5
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-  if [[ "$TRAVIS_BRANCH" = "linux32" ]]; then
-    export PLATFORM="x86"
-  else
-    export PLATFORM="x86_64"
-  fi
   if [[ "$MINICONDA" = "3" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-$PLATFORM.sh -O miniconda.sh;
   else
