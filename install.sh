2cc4fee867daca39aec1664a97b821b4356eac5d
Pierre Fernique <pfernique@gmail.com>
2017-02-01 08:50:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,20 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
   sudo apt-get update
   sudo apt-get install -qq gcc-5 g++-5
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-  if [[ "$MINICONDA" = "3" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  if [[ "$TRAVIS_BRANCH" = "linux32" ]]; then
+    export PLATFORM="x86"
   else
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+    export PLATFORM="x86_64"
   fi
- elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+  if [[ "$MINICONDA" = "3" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-$PLATFORM.sh -O miniconda.sh;
+  else
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-$PLATFORM.sh -O miniconda.sh;
+  fi
+elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+  if [[ "$TRAVIS_BRANCH" = "linux32" ]]; then
+    exit 1
+  fi
   if [[ "$MINICONDA" = "3" ]]; then
     curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o miniconda.sh;
   else
@@ -20,15 +28,6 @@ fi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
 conda config --set always_yes yes --set changeps1 no
-# if [[ ! "$CONDA_CACHE_DIR" = "" ]]; then
-#   echo "conda-build:" >> $HOME/.condarc;
-#   echo "  root-dir: " $CONDA_CACHE_DIR >> $HOME/.condarc;
-#   if [[ ! -f $CONDA_CACHE_DIR/$TRAVIS_BUILD_NUMBER ]]; then
-#     rm -rf $CONDA_CACHE_DIR;
-#     mkdir $CONDA_CACHE_DIR;
-#     touch $CONDA_CACHE_DIR/$TRAVIS_BUILD_NUMBER;
-#   fi
-# fi
 conda update -q conda
 conda info -a
 conda install conda-build
