55a8fbd59b8868dfe2e0dd79efdaf9efbc385ba3
Pierre Fernique <pfernique@gmail.com>
2017-02-01 10:03:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
   sudo apt-get update
   sudo apt-get install -qq gcc-5 g++-5
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-  wget http://statiskit.readthedocs.io/en/latest/_downloads/developer_install.sh -O developer_install.sh;
+  wget https://raw.githubusercontent.com/StatisKit/StatisKit/master/doc/user/user_install.sh -O developer_install.sh;
 elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
-  curl http://statiskit.readthedocs.io/en/latest/_downloads/developer_install.sh -o developer_install.sh;
+  curl https://raw.githubusercontent.com/StatisKit/StatisKit/master/doc/user/user_install.sh -o developer_install.sh;
 fi
 export BATCH_MODE=true
 source developer_install.sh
