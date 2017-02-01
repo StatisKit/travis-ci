633758d85a6a93638345db3d01c93fac5a4a9259
Pierre Fernique <pfernique@gmail.com>
2017-02-01 13:06:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ elif [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
   curl https://raw.githubusercontent.com/StatisKit/StatisKit/master/doc/developer/developer_install.sh -o developer_install.sh;
 fi
 export BATCH_MODE=true
+export ENVIRONMENT=false
 set +e
 source developer_install.sh
 set -ev
