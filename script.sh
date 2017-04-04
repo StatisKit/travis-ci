5d258cc3678880d1ba54cbb30167eecfb54cb6fd
Pierre Fernique <pfernique@gmail.com>
2017-04-04 09:44:26 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,10 @@ set -ev
 if [[ ! "$RECIPE" = "" ]]; then
   if [[ "$TRAVIS_WAIT" = "true" ]]; then
     TRAVIS_WAIT=travis_wait
+  elif [[ ! "$TRAVIS_WAIT" = "" ]]; then
+    TRAVIS_WAIT="travis_wait $TRAVIS_WAIT"
   fi
-  $TRAVIS_WAIT conda build ../conda/$RECIPE -c statiskit -c conda-forge
+  $TRAVIS_WAIT conda build ../conda/$RECIPE $ANACONDA_CHANNELS
 elif [[ ! "$ENVIRONMENT" = "" ]]; then
   conda env update -f ../$ENVIRONMENT
 elif [[ ! "$UBUNTU" = "" ]]; then
