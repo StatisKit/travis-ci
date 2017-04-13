66d5767f5350786f75d1f448ea6ef9be1029c767
Pierre Fernique <pfernique@gmail.com>
2017-04-13 11:03:20 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,11 +4,11 @@ git clone https://github.com/StatisKit/StatisKit.git
 if [[ -f StatisKit/doc/$TRAVIS_ON_NAME_$INSTALL ]]; then
   rm StatisKit/doc/$TRAVIS_ON_NAME_$INSTALL
 fi
-mv $INSTALL StatisKit/doc/$TRAVIS_ON_NAME_$INSTALL
+mv $INSTALL StatisKit/doc/$TRAVIS_OS_NAME_$INSTALL
 cd StatisKit
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-git commit -a -m "Update "$TRAVIS_ON_NAME_$INSTALL" script"
+git commit -a -m "Update "$TRAVIS_OS_NAME_$INSTALL" script"
 echo "machine github.com" >> ~/.netrc
 echo "       login "$GITHUB_USERNAME >> ~/.netrc
 echo "       password "$GITHUB_PASSWORD >> ~/.netrc
