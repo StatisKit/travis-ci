5f9d440a46be8d4eeb223ccaad4cbd205de99ecd
Pierre Fernique <pfernique@gmail.com>
2017-04-13 11:06:20 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -8,10 +8,11 @@ mv $INSTALL StatisKit/doc/$TRAVIS_OS_NAME_$INSTALL
 cd StatisKit
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
+git add doc/$TRAVIS_OS_NAME_$INSTALL
 git commit -a -m "Update "$TRAVIS_OS_NAME_$INSTALL" script"
 echo "machine github.com" >> ~/.netrc
-echo "       login "$GITHUB_USERNAME >> ~/.netrc
-echo "       password "$GITHUB_PASSWORD >> ~/.netrc
+echo "       login newbie"$GITHUB_USERNAME >> ~/.netrc
+echo "       password abc"$GITHUB_PASSWORD >> ~/.netrc
 git push
 
 set +ve
