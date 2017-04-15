b7f0c65e99fdadeaead8b5ef2e02c189b9c24d9f
Pierre Fernique <pfernique@gmail.com>
2017-04-15 10:55:09 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,16 +4,16 @@ git clone https://github.com/StatisKit/StatisKit.git
 if [[ ! -d StatisKit/doc/$TRAVIS_OS_NAME ]]; then
   mkdir StatisKit/doc/$TRAVIS_OS_NAME
 fi
-if [[ -f StatisKit/doc/$TRAVIS_OS_NAME"/"$INSTALL ]]; then
-  rm StatisKit/doc/$TRAVIS_OS_NAME"/"$INSTALL
+if [[ -f "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
+  rm "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
 fi
-mv $INSTALL StatisKit/doc/$TRAVIS_OS_NAME/$INSTALL
-sudo chmod a+rwx StatisKit/doc/$TRAVIS_OS_NAME/$INSTALL
+mv $INSTALL "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+sudo chmod a+rwx "StatisKit/doc/$TRAVIS_OS_NAME"/"$INSTALL"_install"
 cd StatisKit
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-git add doc/$TRAVIS_OS_NAME"/"$INSTALL
-git commit -a -m "Update "$TRAVIS_OS_NAME"/"$INSTALL" script"
+git add "doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+git commit -a -m "Update "$TRAVIS_OS_NAME"/"$INSTALL"_install script"
 echo "machine github.com" >> ~/.netrc
 echo "       login "$GITHUB_USERNAME >> ~/.netrc
 echo "       password "$GITHUB_PASSWORD >> ~/.netrc
