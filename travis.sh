1f60af6118c9b9465466bbca17bc23fbd6013fc8
Pierre Fernique <pfernique@gmail.com>
2017-04-20 15:10:44 +0200

    Change binaries repository

--- travis.sh
+++ travis.sh
@@ -1,14 +1,14 @@
 set +ve
 
-git clone https://github.com/StatisKit/StatisKit.git --depth=1
-if [[ ! -d StatisKit/doc/$TRAVIS_OS_NAME ]]; then
-  mkdir StatisKit/doc/$TRAVIS_OS_NAME
+git clone https://github.com/StatisKit/install-binaries.git --depth=1
+if [[ ! -d $TRAVIS_OS_NAME ]]; then
+  mkdir $TRAVIS_OS_NAME
 fi
-if [[ -f "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
-  rm "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+if [[ -f $TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
+  rm $TRAVIS_OS_NAME"/"$INSTALL"_install"
 fi
-mv $INSTALL"_install" "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
-sudo chmod a+rwx "StatisKit/doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+mv $INSTALL"_install" $TRAVIS_OS_NAME"/"$INSTALL"_install"
+sudo chmod a+rwx $TRAVIS_OS_NAME"/"$INSTALL"_install"
 cd StatisKit
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
