b81f17ec3b947cb171086e321339e3b16996afa7
Pierre Fernique <pfernique@gmail.com>
2017-04-20 15:23:42 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -11,7 +11,7 @@ mv $INSTALL"_install" "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
 cd install-binaries
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-git add "doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+git add $TRAVIS_OS_NAME"/"$INSTALL"_install"
 git commit -a -m "Update "$TRAVIS_OS_NAME"/"$INSTALL"_install script"
 echo "machine github.com" >> ~/.netrc
 echo "       login "$GITHUB_USERNAME >> ~/.netrc
