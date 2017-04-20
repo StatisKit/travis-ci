66f70e8da02fdd3d6b2da636c3910f923b5d2292
Pierre Fernique <pfernique@gmail.com>
2017-04-20 15:14:00 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,15 +1,14 @@
 set +ve
 
 git clone https://github.com/StatisKit/install-binaries.git --depth=1
-if [[ ! -d $TRAVIS_OS_NAME ]]; then
-  mkdir $TRAVIS_OS_NAME
+if [[ ! -d "install-binaries/"$TRAVIS_OS_NAME ]]; then
+  mkdir "install-binaries/"$TRAVIS_OS_NAME
 fi
-if [[ -f $TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
-  rm $TRAVIS_OS_NAME"/"$INSTALL"_install"
+if [[ -f "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
+  rm "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
 fi
-mv $INSTALL"_install" $TRAVIS_OS_NAME"/"$INSTALL"_install"
-sudo chmod a+rwx $TRAVIS_OS_NAME"/"$INSTALL"_install"
-cd StatisKit
+mv $INSTALL"_install" "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+cd install-binaries
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 git add "doc/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
