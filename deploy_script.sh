32fc2eb181397d5aae22bca14e67d8ae2da25d24
Pierre Fernique <pfernique@gmail.com>
2017-07-11 12:10:14 +0200

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,6 +5,11 @@ if [[ "$DOCKER_DEPLOY" = "true" ]]; then
 fi
 
 if [[ "$ANACONDA_DEPLOY" = "true" ]]; then
+  if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then		
+    if [[ ! "$ENVIRONMENT" = "" ]]; then		
+      anaconda upload ../environment.yml -u $ANACONDA_UPLOAD --force		
+    fi
+  fi
   if [[ ! "$RECIPE" = "" ]]; then
       anaconda upload `conda build ../conda/$RECIPE $ANACONDA_CHANNELS --output` -u $ANACONDA_UPLOAD --force
   fi
