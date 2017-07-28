c729dae8e664e4f03a0d0ff4e680859f9eef1312
Pierre Fernique <pfernique@gmail.com>
2017-07-28 11:23:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,25 @@
 set -ev
 
+if [[ "$TRAVIS_WAIT" = "true" ]]; then
+  TRAVIS_WAIT=travis_wait
+elif [[ ! "$TRAVIS_WAIT" = "" ]]; then
+  TRAVIS_WAIT="travis_wait $TRAVIS_WAIT"
+fi
+  
 if [[ ! "$CONDA_RECIPE" = "" ]]; then
-  if [[ "$TRAVIS_WAIT" = "true" ]]; then
-    TRAVIS_WAIT=travis_wait
-  elif [[ ! "$TRAVIS_WAIT" = "" ]]; then
-    TRAVIS_WAIT="travis_wait $TRAVIS_WAIT"
-  fi
-  $TRAVIS_WAIT conda build ../bin/conda/$CONDA_RECIPE $ANACONDA_CHANNELS
+  cd ../bin/conda
+  $TRAVIS_WAIT conda build /$CONDA_RECIPE $ANACONDA_CHANNELS
+  cd ../../travis-ci
 elif [[ ! "$JUPYTER_NOTEBOOK" = "" ]]; then
-  jupyter nbconvert --ExecutePreprocessor.timeout=3600 --to notebook --execute ../share/jupyter/$JUPYTER_NOTEBOOK --output ../share/jupyter/$JUPYTER_NOTEBOOK
+  cd ../share/jupyter
+  $TRAVIS_WAIT jupyter nbconvert --ExecutePreprocessor.timeout=3600 --to notebook --execute $JUPYTER_NOTEBOOK --output $JUPYTER_NOTEBOOK
+  cd ../../travis-ci
 elif [[ ! "$DOCKERFILE" = "" ]]; then
-  mv ../bin/docker/$DOCKERFILE ../bin/docker/Dockerfile
-  docker build -t statiskit/$DOCKERFILE ../bin/docker
-  mv ../bin/docker/Dockerfile ../bin/docker/$DOCKERFILE
+  cd ../bin/docker
+  mv $DOCKERFILE Dockerfile
+  $TRAVIS_WAIT docker build -t statiskit/$DOCKERFILE ../bin/docker
+  mv Dockerfile $DOCKERFILE
+  cd ../../travis-ci
 fi
 
 set +ev
