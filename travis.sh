3b22f7ccf8dccd493248499d62774960375ebf46
Pierre Fernique <pfernique@gmail.com>
2017-09-07 15:03:00 +0200

    Update for Python 3

--- travis.sh
+++ travis.sh
@@ -4,15 +4,17 @@ git clone https://github.com/StatisKit/install-binaries.git --depth=1
 if [[ ! -d "install-binaries/"$TRAVIS_OS_NAME ]]; then
   mkdir "install-binaries/"$TRAVIS_OS_NAME
 fi
-if [[ -f "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
-  rm "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
+if [[ ! -d "install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION ]]; then
+  mkdir "install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION
 fi
-mv $INSTALL"_install-"$PYTHON_VERSION "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
-cd install-binaries
+if [[ -f "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
+  rm "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install"
+fi
+mv $INSTALL"_install install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install"
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-git add $TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
-git commit -a -m "Update "$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION" script"
+git add $TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install"
+git commit -a -m "Update "$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install"
 echo "machine github.com" >> ~/.netrc
 echo "       login "$GITHUB_USERNAME >> ~/.netrc
 echo "       password "$GITHUB_PASSWORD >> ~/.netrc
