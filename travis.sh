d9e37902b1d95a21cf08d3f2592b9e740d3b0469
Pierre Fernique <pfernique@gmail.com>
2017-09-07 15:56:34 +0200

    Update for Python 3

--- travis.sh
+++ travis.sh
@@ -1,3 +1,4 @@
+set +ve
 
 git clone https://github.com/StatisKit/install-binaries.git --depth=1
 if [[ ! -d "install-binaries/"$TRAVIS_OS_NAME ]]; then
@@ -9,16 +10,13 @@ fi
 if [[ -f "install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install" ]]; then
   rm "install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"_install"
 fi
-ls install-binaries/"$TRAVIS_OS_NAME"
 echo $INSTALL"_install install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install"
 mv $INSTALL"_install" "install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION
-ls install-binaries/"$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"
 cd install-binaries
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 git add $TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install"
 git commit -a -m "Update "$TRAVIS_OS_NAME"/PY"$PYTHON_VERSION"/"$INSTALL"_install"
-set +ve
 echo "machine github.com" >> ~/.netrc
 echo "       login "$GITHUB_USERNAME >> ~/.netrc
 echo "       password "$GITHUB_PASSWORD >> ~/.netrc
