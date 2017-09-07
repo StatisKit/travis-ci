e148e1ff488209eeda4e7bd1b72a554ebbd1cb62
Pierre Fernique <pfernique@gmail.com>
2017-09-07 14:53:28 +0200

    Update for Python 3

--- travis.sh
+++ travis.sh
@@ -7,11 +7,11 @@ fi
 if [[ -f "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install" ]]; then
   rm "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
 fi
-mv $INSTALL"_install" "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
+mv $INSTALL"_install-"$PYTHON_VERSION "install-binaries/"$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
 cd install-binaries
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-git add $TRAVIS_OS_NAME"/"$INSTALL"_install"
+git add $TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION
 git commit -a -m "Update "$TRAVIS_OS_NAME"/"$INSTALL"_install-"$PYTHON_VERSION" script"
 echo "machine github.com" >> ~/.netrc
 echo "       login "$GITHUB_USERNAME >> ~/.netrc
