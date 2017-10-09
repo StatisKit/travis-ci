a6f4edf7ff5efbd40dc90f64b26933e65f4e05ef
Pierre Fernique <pfernique@gmail.com>
2017-10-09 08:48:34 +0200

    Update before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -1,17 +1,17 @@
 set -ev
 
 if [[ ! "$CONDA_RECIPE" = "" ]]; then
-	if [[ -f ../bin/conda/$CONDA_RECIPE/travis-ci.patch ]]; then
-		cd ..
-		git apply -v bin/conda/$CONDA_RECIPE/travis-ci.patch
-		cd travis-ci
-	fi
+    if [[ -f ../bin/conda/$CONDA_RECIPE/travis-ci.patch ]]; then
+        cd ..
+        git apply -v bin/conda/$CONDA_RECIPE/travis-ci.patch
+        cd travis-ci
+    fi
 fi
 
 if [[ ! "$CONDA_ENVIRONMENT" = "" ]]; then
-        conda env create -n $CONDA_ENVIRONMENT python=$CONDA_VERSION
-	source activate $CONDA_ENVIRONMENT
-	conda env update -f ../environment.yml
+    conda create -n $CONDA_ENVIRONMENT python=$CONDA_VERSION
+    source activate $CONDA_ENVIRONMENT
+    conda env update -f ../environment.yml
 fi
 
 set +ev
