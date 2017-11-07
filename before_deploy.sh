8d0337ec43e285512791ee579ed73cd54de962d5
Pierre Fernique <pfernique@gmail.com>
2017-11-07 14:26:30 +0100

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,24 +1,30 @@
 set -e
 set +v
 
-DOCKER_DEPLOY="false"
-if [[ "$TRAVIS_OS_NAME" = "linux" &&  ! "$DOCKER_USERNAME" = "" && ! "$DOCKER_PASSWORD" = "" && "$CONDA_RECIPE" = "" && "$CONDA_ENVIRONMENT" = "" ]]; then
-    DOCKER_DEPLOY="true"
+if [[ "$DOCKER_DEPLOY" = "" ]]; then
+    if [[ "$TRAVIS_OS_NAME" = "linux" &&  ! "$DOCKER_USERNAME" = "" && ! "$DOCKER_PASSWORD" = "" && "$CONDA_RECIPE" = "" && "$CONDA_ENVIRONMENT" = "" ]]; then
+        export DOCKER_DEPLOY="true"
+    else
+        export DOCKER_DEPLOY="false"
+    fi
 fi
 if [[ "$DOCKER_DEPLOY" = "true" ]]; then
   docker login -p $DOCKER_PASSWORD -u $DOCKER_USERNAME
 fi
-export DOCKER_DEPLOY=$DOCKER_DEPLOY
 
-ANACONDA_DEPLOY="false"
-if [[ ! "$ANACONDA_USERNAME" = "" && ! "$ANACONDA_PASSWORD" = "" ]]; then
-    if [[ ! "$CONDA_RECIPE" = "" || ! "$CONDA_ENVIRONMENT" = "" ]]; then
-        ANACONDA_DEPLOY="true"
+if [[ "$ANACONDA_DEPLOY" = "" ]]; then
+    if [[ ! "$ANACONDA_USERNAME" = "" && ! "$ANACONDA_PASSWORD" = "" ]]; then
+        if [[ ! "$CONDA_RECIPE" = "" || ! "$CONDA_ENVIRONMENT" = "" ]]; then
+            export ANACONDA_DEPLOY="true"
+        else
+            export ANACONDA_DEPLOY="false"
+        fi
+    else
+        export ANACONDA_DEPLOY="false"
     fi
 fi
 if [[ "$ANACONDA_DEPLOY" = "true" ]]; then
   yes | anaconda login --password $ANACONDA_PASSWORD --username $ANACONDA_USERNAME
 fi
-export ANACONDA_DEPLOY=$ANACONDA_DEPLOY
 
 set +ev
