5fc7bb9deb6bd74bdc52cd10a1dab558ee6a9a22
Pierre Fernique <pfernique@gmail.com>
2017-11-07 16:17:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ elif [[ ! "$JUPYTER_NOTEBOOK" = "" ]]; then
   $TRAVIS_WAIT jupyter nbconvert --ExecutePreprocessor.kernel_name='python'$CONDA_VERSION --ExecutePreprocessor.timeout=0 --to notebook --execute ../$JUPYTER_NOTEBOOK --output ../$JUPYTER_NOTEBOOK
 elif [[ ! "$DOCKERFILE" = "" ]]; then
   mv ../$DOCKERFILE Dockerfile
-  eval $TRAVIS_WAIT" docker build --build-arg CONDA_VERSION=$CONDA_VERSION -t $DOCKER_UPLOAD/"$DOCKER_TAG":"$TRAVIS_TAG"-py"$CONDA_VERSION"k ."
+  eval $TRAVIS_WAIT" docker build --build-arg CONDA_VERSION=$CONDA_VERSION -t $DOCKER_UPLOAD/"$DOCKER_REPOSITORY":"$TRAVIS_TAG"-py"$CONDA_VERSION"k ."
   mv Dockerfile ../$DOCKERFILE
 fi
 
