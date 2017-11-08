1b63db35409937d8716d7b67ae13ce3d4f179221
Pierre Fernique <pfernique@gmail.com>
2017-11-08 16:19:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,23 +53,6 @@ elif [[ ! "$TRAVIS_WAIT" = "" ]]; then
 fi
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  if [[ "$PLATFORM" = "x86" ]]; then
-    sudo apt-get install ia32-libs
-  fi
-  sudo apt-get update
-  sudo apt-get install -qq gcc-5 g++-5
-  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-  if [[ ! "$DOCKERFILE" = "" ]]; then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get -y install docker-ce
-  fi
-  sudo apt-get install gfortran
-fi
-
-if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
   curl "https://repo.continuum.io/miniconda/Miniconda"$CONDA_VERSION"-latest-Linux-x86_64.sh" -o miniconda.sh
 else
   curl "https://repo.continuum.io/miniconda/Miniconda"$CONDA_VERSION"-latest-MacOSX-x86_64.sh" -o miniconda.sh
