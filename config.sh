dbd80d53de628e1cf0da8b99938de1265868432e
Pierre Fernique <pfernique@gmail.com>
2017-11-08 15:07:19 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@ conda config --set always_yes yes
 conda config --add channels r
 conda config --add channels statiskit
 if [[ ! "$ANACONDA_UPLOAD" = "statiskit" ]]; then
+  conda config --add channels statiskit/label/unstable
   conda config --add channels $ANACONDA_UPLOAD
   if [[ ! "$ANACONDA_LABEL" = "main" ]]; then
       conda config --add channels $ANACONDA_UPLOAD/label/$ANACONDA_LABEL
