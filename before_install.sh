46cddbd163fc6d21c422183c2a621f1c9a1bf914
Pierre Fernique <pfernique@gmail.com>
2017-11-20 10:20:08 +0100

    Create before_install.sh

---
+++ before_install.sh
@@ -0,0 +1,8 @@
+echo "$TRAVIS_COMMIT_MESSAGE" | grep -F -q "[skip travis]"
+if [[ "$?" = "0" ]]; then
+    exit 0
+fi
+echo "$TRAVIS_COMMIT_MESSAGE" | grep -F -q "[travis skip]"
+if [[ "$?" = "0" ]]; then
+    exit 0
+fi
