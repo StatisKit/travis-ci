07810da9583400ea6f7ec4b54b25258570b9b9a7
Pierre Fernique <pfernique@gmail.com>
2017-12-06 19:37:08 +0100

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -34,7 +34,7 @@ fi
 
 if [[ "$ANACONDA_DEPLOY" = "true" ]]; then
   if [[ ! "$CONDA_RECIPE" = "" ]]; then
-      anaconda upload `conda build --old-build-string --python=$PYTHON_VERSION ../$CONDA_RECIPE --output` -u $ANACONDA_UPLOAD --force --no-progress --label $ANACONDA_LABEL
+      anaconda upload `conda build --old-build-string --python=$PYTHON_VERSION ../$CONDA_RECIPE --output` -u $ANACONDA_UPLOAD --force --label $ANACONDA_LABEL
   fi
 fi
 if [[ ! "$ANACONDA_RELABEL" = "" && $ANACONDA_RELEASE = "true" ]]; then
