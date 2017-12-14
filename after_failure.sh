7a1c9cfdfb9c67e7076b3623f4d4ddfabf233634
Pierre Fernique <pfernique@gmail.com>
2017-12-14 13:29:32 +0100

    Update after_failure.sh

--- after_failure.sh
+++ after_failure.sh
@@ -33,8 +33,8 @@ echo "TRAVIS_ALLOW_FAILURE: "$TRAVIS_ALLOW_FAILURE
 
 if [[ "$ANACONDA_LABEL" = "travis-release" && ! "$TRAVIS_ALLOW_FAILURE" = "true" ]]; then
     sudo apt-get install python-software-properties -y
-    sudo apt-add-repository ppa:brightbox/ruby-ng
-    sudo apt-get update
+    sudo apt-add-repository ppa:brightbox/ruby-ng -y
+    sudo apt-get update -y
     sudo apt-get install ruby2.1 ruby-switch ruby2.1-dev -y
     gem install travis -v 1.8.8 --no-rdoc --no-ri
     travis cancel
