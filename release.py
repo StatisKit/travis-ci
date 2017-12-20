4840114823e0f39a01aa6a7cbfc0ba13747137c9
Pierre Fernique <pfernique@gmail.com>
2017-12-20 15:11:51 +0100

    Create release.py

---
+++ release.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import os
+
+headers = {"Accept": "application/vnd.travis-ci.2+json"}
+url = "https://api.travis-ci.org/repos/" + os.environ["TRAVIS_REPO_SLUG"] + "/builds/" + os.environ["TRAVIS_BUILD_ID"]
+
+TRAVIS_JOB_NUMBER = int(os.environ["TRAVIS_JOB_NUMBER"][len(os.environ["TRAVIS_BUILD_NUMBER"]) + 1:])
+
+status = requests.get(url, headers=headers).json()
+for job in status['jobs'][:TRAVIS_JOB_NUMBER]:
+    if job["config"]["os"] == os.environ["TRAVIS_OS_NAME"] and not job["allow_failure"] and job["state"] == 'failed':
+        raise Exception("A previous job failed !")
