e6da59cc35ab4c9acf04dbd59ded90f816d50416
Pierre Fernique <pfernique@gmail.com>
2017-12-23 11:05:37 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,6 +22,38 @@
 
 set -ev
   
+travis_wait() {
+  local cmd="$@"
+  local log_file=travis_wait_$$.log
+
+  $cmd 2>&1 >$log_file &
+  local cmd_pid=$!
+
+  travis_jigger $! $cmd &
+  local jigger_pid=$!
+  local result
+
+  { wait $cmd_pid 2>/dev/null; result=$?; ps -p$jigger_pid 2>&1>/dev/null && kill $jigger_pid; } || exit 1
+  exit $result
+}
+
+travis_jigger() {
+  local timeout=20 # in minutes
+  local count=0
+
+  local cmd_pid=$1
+  shift
+
+  while [ $count -lt $timeout ]; do
+    count=$(($count + 1))
+    echo -e "\033[0mStill running ($count of $timeout): $@"
+    sleep 60
+  done
+
+  echo -e "\n\033[31;1mTimeout reached. Terminating $@\033[0m\n"
+  kill -9 $cmd_pid
+}
+
 if [[ ! "$CONDA_RECIPE" = "" ]]; then
   $TRAVIS_WAIT conda build --old-build-string --python=$PYTHON_VERSION ../$CONDA_RECIPE
 elif [[ ! "$JUPYTER_NOTEBOOK" = "" ]]; then
