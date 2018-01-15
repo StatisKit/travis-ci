085cd798d37e0149a1be471cd0b4b23fda62e95e
Pierre Fernique <pfernique@gmail.com>
2018-01-15 13:23:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,6 +107,7 @@ if [[ ! "$ANACONDA_CHANNELS" = "" ]]; then
 fi
 conda config --set always_yes yes
 conda config --set remote_read_timeout_secs 600
+conda config --set auto_update_conda False
 
 conda install conda=4.3.30 conda-build=3.0.30 anaconda-client 
 source activate
