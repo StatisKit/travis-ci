ed6f20e7e41711dfa402bc1dae4876c6337237c1
Pierre Fernique <pfernique@gmail.com>
2018-01-22 11:04:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,10 +75,12 @@ if [[ ! "$DOCKER_CONTEXT" = "" ]]; then
   if [[ "$DOCKER_CONTAINER" = "" ]]; then
     export DOCKER_CONTAINER=`basename $(dirname ..\$DOCKER_CONTEXT)`
   fi
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
-  sudo apt-get -y install docker-ce 
+  if [[ "$CI" = "true" ]]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get -y install docker-ce 
+  fi
 fi
 
 if [[ "$TRAVIS_TAG" = "" ]]; then
