5e259b54e9f23bd3f8395ca6b5315c7b0b80873f
Pierre Fernique <pfernique@gmail.com>
2018-01-22 11:10:09 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,6 +22,8 @@
 
 set -ev
 
+echo $PWD
+
 if [[ ! "$CONDA_RECIPE" = "" ]]; then
   $TRAVIS_WAIT conda build $OLD_BUILD_STRING_ARG --python=$PYTHON_VERSION ../$CONDA_RECIPE
 elif [[ ! "$JUPYTER_NOTEBOOK" = "" ]]; then
