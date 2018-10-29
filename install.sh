9f436a2552df0ccbaa8aad8937296d0711f6e824
Pierre Fernique <pfernique@gmail.com>
2018-10-29 09:47:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ fi
 
 if [[ "$ANACONDA_LABEL" = "" ]]; then
     export ANACONDA_LABEL=main
-fi curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+fi
 
 if [[ ! "$DOCKER_LOGIN" = "" ]]; then
   if [[ "$DOCKER_OWNER" = "" ]]; then
