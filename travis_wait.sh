8884914ad301b0434f4852f684ff70c888ddf72f
Pierre <pfernique@gmail.com>
2018-10-29 15:20:23 +0100

    Update

--- travis_wait.sh
+++ travis_wait.sh
@@ -2,7 +2,26 @@ set -ve
 
 if [[ ! "$TRAVIS_WAIT" = "" ]] && [[ "$CI" = "true" ]]; then
 
-  travis_wait() {
+    travis_jigger() {
+      local cmd_pid="${1}"
+      shift
+      local timeout="${1}"
+      shift
+      local count=0
+
+      echo -e "\\n"
+
+      while [[ "${count}" -lt "${timeout}" ]]; do
+        count="$((count + 1))"
+        echo -ne "Still running (${count} of ${timeout}): ${*}\\r"
+        sleep 60
+      done
+
+      echo -e "\\n${ANSI_RED}Timeout (${timeout} minutes) reached. Terminating \"${*}\"${ANSI_RESET}\\n"
+      kill -9 "${cmd_pid}"
+    }
+
+    travis_wait() {
     local timeout="${1}"
 
     if [[ "${timeout}" =~ ^[0-9]+$ ]]; then
@@ -36,12 +55,12 @@ if [[ ! "$TRAVIS_WAIT" = "" ]] && [[ "$CI" = "true" ]]; then
     fi
 
     return "${result}"
-  }
+    }
 
   if [[ "$TRAVIS_WAIT" = "true" ]]; then
-    export TRAVIS_WAIT=travis_wait 20
+    export TRAVIS_WAIT="travis_wait 20"
   else
-    export TRAVIS_WAIT=travis_wait ${TRAVIS_WAIT}
+    export TRAVIS_WAIT="travis_wait ${TRAVIS_WAIT}"
   fi
 fi
 
