3cdeebfcfaeb0db234d9eb0e38ff0ba1845c15ba
Pierre Fernique <pfernique@gmail.com>
2018-11-19 12:18:27 +0100

    Update

--- conda_feature.py
+++ conda_feature.py
@@ -24,15 +24,14 @@ def main():
         else:
             with open(CONDA_RECIPE, "rb") as filehandler:
                 CONDA_META = yaml.load(filehandler)
-        print(CONDA_META)
         build = CONDA_META.get("build", dict())
         build["features"] = build.get("features", [])
         if environ["CONDA_FEATURE"] not in build["features"]:
             build["features"].append(environ["CONDA_FEATURE"])
+        build["track_features"] = build.get("track_features", [])
         if environ["CONDA_FEATURE"] not in build["track_features"]:
             build["track_features"].append(environ["CONDA_FEATURE"])
         CONDA_META["build"] = build
-        print(CONDA_META)
         if PY2:
             with open(CONDA_RECIPE, "w") as filehandler:
                 CONDA_META = yaml.dump(CONDA_META, filehandler)
