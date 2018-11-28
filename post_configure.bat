3c8367875bec51ba120bf669005d7f32807fdee9
Pierre <pfernique@gmail.com>
2018-11-28 16:34:35 +0100

    Update

--- post_configure.bat
+++ post_configure.bat
@@ -12,25 +12,25 @@ del miniconda.exe
 if errorlevel 1 exit 1
 set PATH=%HOMEDRIVE%\Miniconda;%HOMEDRIVE%\Miniconda\Scripts;%PATH%
 if errorlevel 1 exit 1
-call activate.bat
+conda activate
 if errorlevel 1 exit 1
 if not "%ANACONDA_CHANNELS%" == "" (
-  conda.exe config %ANACONDA_CHANNELS%
+  conda config %ANACONDA_CHANNELS%
   if errorlevel 1 exit 1
 )
-conda.exe config --set always_yes yes
+conda config --set always_yes yes
 if errorlevel 1 exit 1
-conda.exe config --set remote_read_timeout_secs 600
+conda config --set remote_read_timeout_secs 600
 if errorlevel 1 exit 1
-conda.exe config --set auto_update_conda False
+conda config --set auto_update_conda False
 if errorlevel 1 exit 1
 
-if not "%CONDA_PIN%" == "" conda.exe install conda=%CONDA_PIN%
+if not "%CONDA_PIN%" == "" conda install conda=%CONDA_PIN%
 if not "%CONDA_BUILD_PIN%" == "" (
-  conda.exe install conda-build=%CONDA_BUILD_PIN%
+  conda install conda-build=%CONDA_BUILD_PIN%
   if errorlevel 1 exit 1 
 ) else (
-  conda.exe install conda-build
+  conda install conda-build
   if errorlevel 1 exit 1
 )
 
@@ -39,10 +39,10 @@ if "%CI%" == "true" (
   if errorlevel 1 exit 1
 )
 if not "%ANACONDA_CLIENT_PIN%" == "" (
-    conda.exe install anaconda-client=$ANACONDA_CLIENT_PIN
+    conda install anaconda-client=$ANACONDA_CLIENT_PIN
     if errorlevel 1 exit 1
 ) else (
-    conda.exe install anaconda-client
+    conda install anaconda-client
     if errorlevel 1 exit 1
 )
 anaconda config --set auto_register yes
@@ -51,15 +51,15 @@ if errorlevel 1 exit 1
 set CMD_IN_ENV=cmd /E:ON /V:ON /C %cd%\\cmd_in_env.cmd
 if errorlevel 1 exit 1
 
-conda.exe create -n py%CONDA_VERSION%k python=%PYTHON_VERSION%
+conda create -n py%CONDA_VERSION%k python=%PYTHON_VERSION%
 if errorlevel 1 exit 1
-call activate.bat py%CONDA_VERSION%k
+conda activate py%CONDA_VERSION%k
 if errorlevel 1 exit 1
 
 if not "%CONDA_PACKAGES%" == "" (
-  conda.exe install  -n py%CONDA_VERSION% %CONDA_PACKAGES% --use-local
+  conda install  -n py%CONDA_VERSION% %CONDA_PACKAGES% --use-local
   if errorlevel 1 exit 1
-  call activate.bat py%CONDA_VERSION%
+  conda activate py%CONDA_VERSION%
   if errorlevel 1 exit 1
 )
 
