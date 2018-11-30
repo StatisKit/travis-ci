7c85c09e5bdbcda40326e1e84b3c16fe0181d8af
Pierre Fernique <pfernique@gmail.com>
2018-11-30 11:46:03 +0100

    Update

--- anaconda_packages.py
+++ anaconda_packages.py
@@ -20,9 +20,15 @@ def main():
         CONDA_PREFIX += "32"
     else:
         CONDA_PREFIX += "64"
-    environ["ANACONDA_SUCCESS_PACKAGES"] = " ".join(os.path.join(CONDA_PREFIX, package) for package in os.listdir(CONDA_PREFIX) if package.endswith(".tar.bz2"))
+    if os.path.exists(CONDA_PREFIX):
+        environ["ANACONDA_SUCCESS_PACKAGES"] = " ".join(os.path.join(CONDA_PREFIX, package) for package in os.listdir(CONDA_PREFIX) if package.endswith(".tar.bz2"))
+    else:
+        environ["ANACONDA_SUCCESS_PACKAGES"] = ""
     CONDA_PREFIX = os.path.join(os.path.abspath(environ["CONDA_PREFIX"]), 'conda-bld', "broken")
-    environ["ANACONDA_FAILURE_PACKAGES"] = " ".join(os.path.join(CONDA_PREFIX, package) for package in os.listdir(CONDA_PREFIX) if package.endswith(".tar.bz2"))
+    if os.path.exists(CONDA_PREFIX):
+        environ["ANACONDA_FAILURE_PACKAGES"] = " ".join(os.path.join(CONDA_PREFIX, package) for package in os.listdir(CONDA_PREFIX) if package.endswith(".tar.bz2"))
+    else:
+        environ["ANACONDA_FAILURE_PACKAGES"] = ""
     if environ["TRAVIS_OS_NAME"] == "windows":
         with open("anaconda_packages.bat", "a+") as filehandler:
             filehandler.write("\n")
